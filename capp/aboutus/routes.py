ca677f0e45810cc65ecf66042e26156e8b7f4f4f
smazouz1810-ui <smazouz.1810@gmail.com>
2025-11-21 14:34:41 +0100

    Delete capp directory

--- capp/aboutus/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template, Blueprint
-
-aboutus=Blueprint('aboutus',__name__)
-
-@aboutus.route('/aboutus')
-def aboutus_home():
-  return render_template('aboutus.html', title='aboutus')

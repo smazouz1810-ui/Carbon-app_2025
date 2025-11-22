265cb5657cabd226e3b44ad7a6e0b452d45d7d02
smazouz1810-ui <smazouz.1810@gmail.com>
2025-11-22 19:49:10 +0100

    Delete capp directory

--- capp/foryou/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-foryou = Blueprint('foryou', __name__, template_folder='templates')
-
-@foryou.route('/foryou')
-def foryou_home():
-    return render_template('foryou.html')
--- capp/home/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-# Création du Blueprint
-home = Blueprint('home', __name__)
-
-# Import des routes pour que Flask les connaisse
-from . import routes
